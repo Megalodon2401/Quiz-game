7d41eec7acaf3e32fe3169b993c622ccb0952a39
Megalodon2401 <74548884+Megalodon2401@users.noreply.github.com>
2021-02-21 14:57:41 +0530

    Add files via upload

---
+++ Quiz.js
@@ -0,0 +1,17 @@
+class Quiz{
+constructor(){}
+display();
+this.title.html("MyQuiz Game");
+this.title. position(350, 0);
+
+this.question.html("Questiom: Which animal has 3 hearts ?");
+this.question.position(150, 80);
+this.option1.html("1: Octopus");
+this.option1.postion(150, 100);
+this.option2.html("2: Deer");
+this.option2.postion(150, 120);
+this.option3.html("3: Tiger");
+this.option3.postion(150, 140);
+
+this.input1.position(150, 230)
+}
